91e8c32370c2df02efedba989d86bdddef8807bf
dklimpel <5740567+dklimpel@users.noreply.github.com>
2020-05-08 21:13:06 +0200

    up

--- tests/rest/admin/test_user.py
+++ tests/rest/admin/test_user.py
@@ -974,8 +974,8 @@ class DeviceRestTestCase(unittest.HomeserverTestCase):
         self.assertEqual(200, channel.code, msg=channel.json_body)
         self.assertEqual("@user:test", channel.json_body["user_id"])
         # Check that all fields are available
-        for d in channel.json_body:
-            #self.assertIn("user_id", d)
+        for d in channel.json_body[]:
+            self.assertIn("user_id", d)
             self.assertIn("device_id", d)
             self.assertIn("display_name", d)
             self.assertIn("last_seen_ip", d)
