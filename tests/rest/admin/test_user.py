26f71c0a4ec74a4a5a0f73209a3206b55d8cc377
dklimpel <5740567+dklimpel@users.noreply.github.com>
2020-05-09 22:09:11 +0200

    up

--- tests/rest/admin/test_user.py
+++ tests/rest/admin/test_user.py
@@ -1203,7 +1203,8 @@ class DeleteDevicesRestTestCase(unittest.HomeserverTestCase):
         self.assertEqual(number_devices, len(res))
         device_ids = ', '.join(str(d["device_id"]) for d in res)
 
-        body = json.dumps({"devices": [', '.join(mylist)]})
+        self.assertEqual(number_devices, {"devices": [device_ids]})
+        body = json.dumps({"devices": [device_ids]})
         request, channel = self.make_request(
             "POST",
             self.url,
