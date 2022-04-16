de22e6908f587d825c947aa3e934f2bcecbdc026
dklimpel <5740567+dklimpel@users.noreply.github.com>
2022-04-16 19:36:00 +0200

    up

--- synapse/storage/databases/main/presence.py
+++ synapse/storage/databases/main/presence.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import TYPE_CHECKING, Dict, Iterable, List, Tuple, cast
+from typing import TYPE_CHECKING, Any, Dict, Iterable, List, Tuple, cast
 
 from synapse.api.presence import PresenceState, UserPresenceState
 from synapse.replication.tcp.streams import PresenceStream
